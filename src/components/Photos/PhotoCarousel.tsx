e71d4521d3ac6c642a10cd2a74176f81aa235786
Emma Craig <emma.craig@getflowbox.com>
2024-02-20 18:08:46 +0100

    add basic carousel

--- src/components/Photos/PhotoCarousel.tsx
+++ src/components/Photos/PhotoCarousel.tsx
@@ -1,17 +1,40 @@
-import { Box, Button, Slide, Stack } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import { useContext, useState } from "react";
+import PhotoContext from "../../contexts/PhotoContext";
+import { responseType } from "../../types/data";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 
 const PhotoCarousel = () => {
+  const { photos } = useContext(PhotoContext) as responseType;
+  const [currPhoto, setCurrPhoto] = useState(0);
+  const nextPhoto = () => {
+    setCurrPhoto((prevPhoto) =>
+      prevPhoto === photos.length - 1 ? 0 : prevPhoto + 1
+    );
+  };
+  const prevPhoto = () => {
+    setCurrPhoto((prevPhoto) =>
+      prevPhoto === 0 ? photos.length - 1 : prevPhoto - 1
+    );
+  };
   return (
     <Box>
-      <Button></Button>
+      <Button onClick={prevPhoto}>
+        <ArrowBackIos />
+      </Button>
       <Box>
-        <Box>
-          <Slide>
-            <Stack></Stack>
-          </Slide>
-        </Box>
+        <Box
+          style={{
+            width: "300px",
+            height: "300px",
+            margin: '10px auto',
+            backgroundImage: `url(${photos[currPhoto].src.medium})`,
+          }}
+        ></Box>
       </Box>
-      <Button></Button>
+      <Button onClick={nextPhoto}>
+        <ArrowForwardIos />
+      </Button>
     </Box>
   );
 };
